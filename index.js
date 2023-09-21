fc66a1cd512746bbf951fc2ba7f25de1b3be2ebc
Gad-Ongoro <gadongoro1@gmail.com>
2023-09-21 16:09:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let multiply, random, mod, max;
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+multiply = num1 * num2;
+
+random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 1004;
+let num4 = 1000;
+mod = num3 % num4;
+
+max = Math.max(20, 18, 16, 14, 12, 10, 8, 6, 4, 2, 0);
